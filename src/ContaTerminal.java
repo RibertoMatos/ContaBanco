ab745ce78add4a904e7130c28eee5767465364b5
RibertoMatos <riba_matos@yahoo.com.br>
2024-08-13 17:55:22 -0300

    Adicionado comentarios

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 public class ContaTerminal {
     
     public static void main(String[] args) {
+        //Conhecer e importar a classe Scanner
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        //Exibir as mensagens para o nosso usuario
+        //Obeter pela scanner os valores difitados no terminal
         System.out.println("Por favor, Digite o numero da sua Conta: ");
         int conta = scanner.nextInt();
 
@@ -22,7 +25,7 @@ public class ContaTerminal {
         System.out.println("Digite seu Saldo: ");
         double saldo = scanner.nextDouble();
 
-        //Imprimindo os dados obitidos
+        //exibir a mensagem conta criada
     System.out.println("Olá " + nome + Sobrenome+ ", obrigado por criar uma conta em nosso banco, sua agência é: " +agencia+ ",conta: " +conta+ " e seu saldo: " + saldo + " já está disponível para saque.");
     
     }
